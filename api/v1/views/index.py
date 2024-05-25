cffb845281f94db608890e2a90671e607d34f52a
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-25 23:22:50 +0300

    task-4.1

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,10 +15,10 @@ def get_status():
 def get_stats():
     stats = {
         "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"), 
-        "places": storage.count("Place"), 
-        "reviews": storage.count("Review"), 
-        "states": storage.count("State"), 
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
         "users": storage.count("User")
     }
     return jsonify(stats)
