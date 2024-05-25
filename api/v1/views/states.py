70229da09368acb5d98b65ce1698c88cf1873237
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-26 00:53:42 +0300

    task-6

---
+++ api/v1/views/states.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""A view for State objects."""
+from api.v1.views import app_views
+from flask import jsonify, abort, Response, request
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Gets state objects."""
+    states = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if state:
+        storage.delete(state)
+        storage.save()
+        return jsonify({})
+    else:
+        abort(404)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Creates a new state object."""
+    if not request.get_json:
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    state = State(**request.get_json())
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    state = storage.get(State, state_id)
+    if state:
+        data = request.get_json()
+        if not data:
+            abort(400, 'Not a JSON')
+        for key, value in data.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(state, key, value)
+        storage.save()
+        return jsonify(state.to_dict()), 200
+    else:
+        abort(404)
