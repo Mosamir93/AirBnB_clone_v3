d10aa8483be3c90d9979cd5bb9555205f0766087
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-26 14:54:04 +0300

    task-8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A view for State objects."""
+"""A view for City objects."""
 from api.v1.views import app_views
 from flask import jsonify, abort, Response, request
 from models import storage
